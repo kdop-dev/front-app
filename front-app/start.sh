434830928385c12cf353a2f1a664b13462c49eee
Anderson Santos <adsantos@gmail.com>
2020-09-18 20:40:58 -0300

    minor fix

--- front-app/start.sh
+++ front-app/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t kdop/front-app:0.0.1 .
-docker push kdop/front-app:0.0.1
-docker run --rm -p 5001:5001 --name=front-app kdop/front-app:0.0.1
+docker build -t kdop/front-app:0.0.3 .
+docker push kdop/front-app:0.0.3
+docker run --rm -p 5000:5000 --name=front-app kdop/front-app:0.0.3
